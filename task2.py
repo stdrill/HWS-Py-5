c3672c32bc40f0f201ce14bf8f06aa213aba73a1
stdrill <tanya23.96@mail.ru>
2022-11-08 23:35:06 +0300

    task 1 and task 2 added

---
+++ task2.py
@@ -0,0 +1,31 @@
+# Создайте программу для игры с конфетами человек против человека.
+# Условие задачи: На столе лежит 2021 конфета. Играют два игрока делая ход друг после друга. 
+# Первый ход определяется жеребьёвкой. За один ход можно забрать не более чем 28 конфет. 
+# Все конфеты оппонента достаются сделавшему последний ход. 
+import random
+
+quant = 30
+lst = ["Таня", "Мартин"]
+rnd = random.randint(0, len(lst) - 1)
+winner = 0
+
+def Game(quant, id):
+    if quant > 0:
+        print(lst[id] + ", введите число: ")
+        playerNum = int(input())
+        if playerNum > 0 and playerNum <= 28 and playerNum <= quant:
+            quant = quant - playerNum
+            if id == 0: id = 1 
+            else: id = 0
+            print(quant)            
+            Game(quant, id)
+        elif playerNum > quant:
+            print("Введите число от 1 до " + str(quant))
+            Game(quant, id)
+        else:
+            print("Вы ввели некорректное число! Выберите число от 1 до 28")
+            Game(quant, id)
+    else:
+        print(lst[id - 1] + ", поздравляю с победой!")
+
+Game(quant, rnd)
